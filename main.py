10010cedcfd20b2682f9eeebf08fb441535f6255
Lyubomir Romanov <97829758+romanov-lyubomir@users.noreply.github.com>
2023-02-06 15:21:27 +0200

    Test

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ print(3)
 print("Merge conflict solved")
 print("Test commit")
 print("Test commit 2")
+
+
+# Q: What is the difference between commit and push?
+# A: Commit is like saving the changes to your local repository. Push is like uploading the changes to the remote repository.
+
+# Q: What is a git pull request?
+# A: A pull request is a request to merge your changes to the main branch.
+
+# Q: What is the difference between merge and pull?
+# A: Merge is like merging your changes to the main branch. Pull is like downloading the changes from the remote repository.
+
+# Q: What is git fetch?
+# A: Git fetch is like downloading the changes from the remote repository without merging them.
